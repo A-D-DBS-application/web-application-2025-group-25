c5b2547b71e38300c43091b952d13b8da21d42cd
irene857 <irene.vereecken@ugent.be>
2025-11-10 16:15:45 +0100

    help

--- Documents/app/config.py
+++ Documents/app/config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = 'Groep25&groep25'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres: Groep25&groep25@db.hsjjntcivrvlguqfdqxl.supabase.co:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:H3F9WpcVHq8SjY9H@db.hsjjntcivrvlguqfdqxl.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
